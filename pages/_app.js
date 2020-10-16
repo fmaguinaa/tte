c465e1c158d074ec58e6df9ec9f837c3df74179d
Franz Maguiña <frz_m@LAPTOP-991HGHVU.(none)>
2020-10-15 22:40:07 -0500

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
 import App, { Container } from 'next/app'
-import Router from 'next/router';
+// import Router from 'next/router';
 import React from 'react'
 
-import NProgress from 'nprogress'; //nprogress module
-import 'nprogress/nprogress.css'; //styles of nprogress
+// import NProgress from 'nprogress'; //nprogress module
+// import 'nprogress/nprogress.css'; //styles of nprogress
 
 import { LocaleProvider } from 'antd'
 import 'moment/locale/es'
 import esEs from 'antd/lib/locale-provider/es_ES'
 
 //Binding events. 
-Router.events.on('routeChangeStart', () => {console.log("jjj");NProgress.start()});
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+// Router.events.on('routeChangeStart', () => {console.log("jjj");NProgress.start()});
+// Router.events.on('routeChangeComplete', () => NProgress.done());
+// Router.events.on('routeChangeError', () => NProgress.done());
 
 class MyApp extends App {
   render () {
