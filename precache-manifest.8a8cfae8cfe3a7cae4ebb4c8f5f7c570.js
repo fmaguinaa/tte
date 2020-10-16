e87bf284b2684fc7e7bbedad7117078af61fa00e
Franz Maguiña <frz_m@LAPTOP-991HGHVU.(none)>
2020-10-16 11:55:43 -0500

    Updates

---
+++ precache-manifest.8a8cfae8cfe3a7cae4ebb4c8f5f7c570.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ba90931b3ad324d5ef09337295854d",
+    "url": "/tte/index.html"
+  },
+  {
+    "revision": "98cf09225da8a2e25ac4",
+    "url": "/tte/static/css/3.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "b1fd242e6007e79e166a",
+    "url": "/tte/static/css/main.dcc2d496.chunk.css"
+  },
+  {
+    "revision": "36f9f637400494b9a756",
+    "url": "/tte/static/js/0.1b603ebb.chunk.js"
+  },
+  {
+    "revision": "98cf09225da8a2e25ac4",
+    "url": "/tte/static/js/3.054858bf.chunk.js"
+  },
+  {
+    "revision": "f6d602a9f41c601430b83470fdaa2076",
+    "url": "/tte/static/js/3.054858bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90981aedda02e328c53d",
+    "url": "/tte/static/js/4.40e594bc.chunk.js"
+  },
+  {
+    "revision": "0f80f2e0feb4a9f3d6fa",
+    "url": "/tte/static/js/5.5ac725e9.chunk.js"
+  },
+  {
+    "revision": "13777ce85a439bf72aa9",
+    "url": "/tte/static/js/6.8409251e.chunk.js"
+  },
+  {
+    "revision": "f41270eed0c01a9cd3e1",
+    "url": "/tte/static/js/7.69602631.chunk.js"
+  },
+  {
+    "revision": "ea45c0798fd38186715b",
+    "url": "/tte/static/js/8.970f3045.chunk.js"
+  },
+  {
+    "revision": "5b6a3f1ca6e193597e4d49c7a42851aa",
+    "url": "/tte/static/js/8.970f3045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0678290de6645396e651",
+    "url": "/tte/static/js/9.7bfaa23f.chunk.js"
+  },
+  {
+    "revision": "b1fd242e6007e79e166a",
+    "url": "/tte/static/js/main.47ccb0ed.chunk.js"
+  },
+  {
+    "revision": "4eef92808f2ac1781cd0",
+    "url": "/tte/static/js/runtime-main.583b85ec.js"
+  },
+  {
+    "revision": "3f8da984ab118d826592cafff85af598",
+    "url": "/tte/static/media/image1.3f8da984.jpg"
+  },
+  {
+    "revision": "f87e45e2337161a378c8963f26d5ba86",
+    "url": "/tte/static/media/logo.f87e45e2.jpg"
+  },
+  {
+    "revision": "0bf351474992a1a2eb729582c0481624",
+    "url": "/tte/static/media/planta1.0bf35147.jpg"
+  },
+  {
+    "revision": "a123712cdb9d83f2dbd0005a34dea9e1",
+    "url": "/tte/static/media/selima_.a123712c.ttf"
+  },
+  {
+    "revision": "0bfca265d318fb8758db24099443290e",
+    "url": "/tte/static/media/tte.0bfca265.png"
+  }
+]);
